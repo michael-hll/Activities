e8017c12d5cf0a191e6ff3cad2ec636a53d38cae
Michael Han <michael_hll@hotmail.com>
2022-12-23 15:32:37 +0800

    end of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 //using Microsoft.EntityFrameworkCore;
 // create Kestrel Server as the host
 // and read appsettings.json & appsettings.Development.json
@@ -8,19 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 /*...............................*/
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
-
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 /*.................................*/
